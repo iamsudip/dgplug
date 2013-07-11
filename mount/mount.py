a896ffb02ca8130b94b0a4d7061188c4af1c584b
Iam sudip <iamsudip@programmer.net>
2013-07-11 14:18:36 +0530

    Changed code completely

--- mount/mount.py
+++ mount/mount.py
@@ -1,6 +1,13 @@
 #!/usr/bin/env python
 
-f=open("/proc/mounts") 				#Opening the mounts file.
-for x in f:
-    print x, 						#Iterating through its contents and printing it line by line.
-f.close() 							#Closing the file
+f=open("/proc/mounts")                                           #Opening the mounts file.
+
+def func(inp):                                                   #Function to separate ine inputs
+    a, b, c, d, e, f=inp                                         #Separating inputs as different words
+    return "%s on %s type %s (%s)" % (a, b, c, d)                #Returning the value as user need
+
+for x in f:                                                      #Iterating through each line
+    s=x.split(" ")                                               #Spliting words ie using spaces
+    print func(s)                                                #Calling func(inp) Printing return value
+
+f.close()                                                        #Closing the file
