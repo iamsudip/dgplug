add7a4c33378abd7088646ca77c9ad9aaa0fed56
Iam sudip <iamsudip@programmer.net>
2013-07-12 17:32:33 +0530

    Will take more NASADQ symbol at one go

--- sharevalue/sharevalue.py
+++ sharevalue/sharevalue.py
@@ -11,20 +11,43 @@ def main(nasdaq):
     :arg nasdaq: NASDAQ symbol
     """
 
-    #making the link in 'link' string to fetch data'
+    #Making the link in 'link' string to fetch data
     link = 'http://download.finance.yahoo.com/d/quotes.csv?s='+nasdaq+'&f=l1'
-    print 'Fetching data from: '+link
+    print 'Fetching recent share value of '+nasdaq
 
-    #data saved into an instance share_value
+    #share value saved into an instance share_value
     share_value = urllib2.urlopen(link)
 
-    #Printing share_value
-    print 'Current share price of company '+nasdaq+': '+share_value.read()
-
-if __name__=='__main__':
-    if len(sys.argv) == 2:
-        main(sys.argv[1])
-    else:
-        print "Syntax: ./sharevalue.py [NASDAQ Symbol]"    
-    sys.exit(0)
+    #Fetching data to find company name
+    html_data=urllib2.urlopen('http://www.nasdaq.com/symbol/'+nasdaq)
+    name=html_data.read()
     
+    #Finding the Company name & Displaying
+    start=name.find('<title>')+7
+    end=name.find('</title>')-25
+    while start < end:
+        print name[start],
+        start+=1
+
+    #Printing share_value which was saved ti share_value
+    print '\n'+'Current share price of company '+nasdaq+': '+share_value.read()
+    share_value.close()
+
+if __name__ == '__main__':
+    
+    #Counting the NASDAQ Symbol (Arguments)
+    count = len(sys.argv) - 1 
+    
+    #Checking if atleast one symbol available or not
+    if count >= 1:
+        
+        #NASDAQ symbol available passing it to main() function 
+        i=0
+        while i < count:
+            i+=1
+            main(sys.argv[i])
+    
+    else:
+        #If nothing given as argument it will print the syntax to use this code"
+        print 'Atleast 1 Argument needed'+'\n'+'Syntax: ./sharevalue.py [NASDAQ Symbol] [NASDAQ Symbol]'   
+    sys.exit(0)
