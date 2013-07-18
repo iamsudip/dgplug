2207812ab20a7126cf2bcef1d48295a4e971de0c
Iam sudip <iamsudip@programmer.net>
2013-07-18 21:43:43 +0530

    Modied code using requests liabrary

--- myshellv1/sharevalue.py
+++ myshellv1/sharevalue.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 
-import urllib2
+import requests
 import sys
 
 def share(nasdaq):
@@ -15,9 +15,21 @@ def share(nasdaq):
     link = 'http://download.finance.yahoo.com/d/quotes.csv?s='+nasdaq+'&f=l1'
     print 'Fetching recent share value of '+nasdaq
 
-    #share value saved into an instance share_value
-    share_value = urllib2.urlopen(link)
+    share_value = requests.get(link)
 
-    #Printing share_value which was saved ti share_value
-    print '\n'+'Current share price of company '+nasdaq+': '+share_value.read()
+    print '\n'+'Current share price of company '+nasdaq+': '+share_value.text
     share_value.close()
+
+if __name__ == '__main__':
+    
+    #Counting the NASDAQ Symbol (Arguments)
+    count = len(sys.argv) - 1 
+    
+    #Checking if atleast one symbol available or not
+    if count >= 1:
+        
+        #NASDAQ symbol available passing it to main() function 
+        i=0
+        while i < count:
+            i+=1
+            share(sys.argv[i])
